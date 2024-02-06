5b5c50f346d4f91dfdb46227822cee6c13794584
Osama Albahrani <54853250+osalbahr@users.noreply.github.com>
2024-02-06 16:21:10 -0500

    use a file descriptor

--- contrib/command-not-found/pk-command-not-found.c
+++ contrib/command-not-found/pk-command-not-found.c
@@ -1,8 +1,13 @@
 #include <glib.h>
 #include <unistd.h>
+#include <stdio.h>
 
 int main(int argc, char *argv[])
 {
+	FILE *fp = fopen("pk-command-not-found.c", "r");
+	int fd = fileno(fp);
 	g_spawn_async_with_fds (NULL, argv + 1, NULL, G_SPAWN_SEARCH_PATH, NULL, NULL, NULL,
-											STDIN_FILENO, STDOUT_FILENO, STDERR_FILENO, NULL);
+											fd, STDOUT_FILENO, STDERR_FILENO, NULL);
+											// STDIN_FILENO, STDOUT_FILENO, STDERR_FILENO, NULL);
+	fclose(fp);
 }
